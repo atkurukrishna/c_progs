83e60fc10d0783086bf07a528e7ee068ea9fbb58
atkurukrishna <krishna.atkuru@gmail.com>
2011-05-18 16:03:46 -0700

    atkurukrishna:c:insertion sort

---
+++ insertion_sort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+
+/* program to do insertion sort and measure properties */
+/*
+while(true) {
+printf("--------------Select one of the options from the menu----\n");
+*/
+int array[8] = {3, 41, 52, 26, 38, 57, 9, 49};
+int i = 0;
+
+for (i = 0; i < 8; i++) {
+  printf("%d ", array[i]);
+}
+
+return 0;
+}
+
+
