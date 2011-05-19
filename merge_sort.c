8969693d4b93c6d6a87a536df340172fc294f0d1
atkurukrishna <krishna.atkuru@gmail.com>
2011-05-19 10:31:28 -0700

    Merge sort - the working version :)

--- merge_sort.c
+++ merge_sort.c
@@ -1,19 +1,12 @@
 #include<stdio.h>
 
-/* Recursive way to do merge sort */
-
-
 void merge(int *array, int start1, int end1, int start2, int end2) {
-  int s1 = start1;
-  int s2 = start2;
-  int e1 = end1;
-  int e2 = end2;
   int temp_array[end2 - start1 + 1];
   int arr_cnt = 0;
-  int p1 = s1;
-  int p2 = s2;
+  int p1 = start1;
+  int p2 = start2;
 
-  while (p1 < s2 && p2 <= e2) {
+  while (p1 < start2 && p2 <= end2) {
     if (*(array + p1) > *(array + p2)) {
       temp_array[arr_cnt++] = *(array + p2);
       p2++;
@@ -22,47 +15,48 @@ void merge(int *array, int start1, int end1, int start2, int end2) {
       p1++;
     }
   }
-  if (p1 < s2) {
-    while (p1 < s2) {
+  if (p1 < start2) {
+    while (p1 < start2) {
       temp_array[arr_cnt++] = *(array + p1);
       p1++;
     }
-  } else if (p2 <= e2) {
-    while (p2 <= e2) {
+  } else if (p2 <= end2) {
+    while (p2 <= end2) {
       temp_array[arr_cnt++] = *(array + p2);
       p2++;
     }
   } else {
-  / * do nothing */
   }
 
   for (p1 = start1, arr_cnt = 0; p1 <= end2; p1++) {
-    *(array + p1++) = temp_array[arr_cnt++];
+    *(array + p1) = temp_array[arr_cnt++];
   }
 }
   
-/* function takes the following:
-  *array - pointer to the array
-  start - begenning of the array
-  end - end of the array
-*/
 void merge_sort(int *array, int start, int end) {
+  int mid_point;
+
   if (start < end) {
-    merge_sort(array, start, (start + end) / 2);
-    merge_sort(array, ((start + end) / 2 ) + 1, end);
-    merge(array, start, (start + end) / 2, ((start + end) / 2) + 1, end);
+    mid_point = (start + end) / 2;   
+    merge_sort(array, start, mid_point);
+    merge_sort(array, mid_point + 1, end);
+    merge(array, start, mid_point, mid_point + 1, end);
   }
   else {
     return ;
   }
 }
  
-
-
 int main() {
+  int i = 0;
   int array[10] = {1, 5, 3, 9, 7, 4, 2, 8, 6, 0};
+  for (; i < 10; i++) {
+    printf("%d ", array[i]);
+  }
   merge_sort(array, 0, 9);
-  
-
-  /* write merge sort to sort this array */
-  
+  printf("\n");
+  for (i = 0; i < 10; i++) {
+    printf("%d ", array[i]);
+  }
+  return 0;
+}
